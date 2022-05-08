192ece50b0b7757c497a02059144fdaef040e09e
Dragon <104348282+icodragon@users.noreply.github.com>
2022-05-08 22:51:53 +1000

    Update install_backup.sh

--- install_backup.sh
+++ install_backup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-wget dragonapi.space/data_06052022.tar
+wget dragonapi.space/last_data_defund.tar
 systemctl stop defund && defundd tendermint unsafe-reset-all
-mv data_06052022.tar $HOME/.defund/
+mv last_data_defund.tar $HOME/.defund/
 cd $HOME/.defund/
-tar xfv data_06052022.tar
+tar xfv last_data_defund.tar
 wget dragonapi.space/addrbook.json && mv addrbook.json $HOME/.defund/config/
 sudo systemctl start defund && sudo journalctl -u defund.service -f -o cat
-cd $HOME/.defund && rm -rf data_06052022.tar
+cd $HOME/.defund && rm -rf last_data_defund.tar
